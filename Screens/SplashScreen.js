5cd262be8667d6260ec4fd6ef8d96fd16f22d0e7
Ragavi <ragvi.r@zucisystems.com>
2021-08-25 06:19:54 +0530

    cmmit

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -1,89 +1,74 @@
-import React , {useState, useEffect, useCallback} from "react"
-import { StyleSheet, View, Text, ImageBackground} from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import ReactNativeBiometrics from 'react-native-biometrics';
 import * as LocalAuthentication from 'expo-local-authentication';
 
-export default function  SplashScreen ({navigation}) {
+
+export default function LoginScreen({navigation}) {
+  const [h_message, seth_message] = useState ("")
+  const [s_message, sets_message] = useState ("")
+  const [e_message, sete_message] = useState ("")  
+  const [p_message, setp_message] = useState ("")  
+  // const [navigates, setnavigates] = useState (false)   
   
-    const [h_message, seth_message] = useState ("")
-    const [s_message, sets_message] = useState ("")
-    const [e_message, sete_message] = useState ("")
-    
-    const [p_message, setp_message] = useState ("")
-
-     
-   
-    const getBiometric = async () => {
-
-       //check for the elegibility of the device
+  const BioCheck = () => {
     const hasHardwareAsync =  LocalAuthentication.hasHardwareAsync();
-    hasHardwareAsync.then((value)=>{
-      if(value){
-        seth_message("Device Elegible for Biometric Auth")
-        const isenrolled = LocalAuthentication.isEnrolledAsync();
-        isenrolled.then((value) => {
-          if(value){
-            sete_message("Device registered with signature")
-            const support = LocalAuthentication.supportedAuthenticationTypesAsync()
-            support.then((value)=>{
-              if(value != undefined){
-                sets_message("Supported level => ", value , " <= , 1- fingerprint,2-face,3-iris")
-                const getEnrolled = LocalAuthentication.authenticateAsync()
-                getEnrolled.then((value)=> {
-                  if(value.success){
-                    navigation.navigate("Home")
-                  }else{
-                    setp_message("Validation Fails => ")
-                    if(value.error == 'user_cancel'){
-                      setp_message(p_message,"User Cancelled the  Prompt")
-                    }else{
-                      if(value.error == 'system_cancel'){
-                        setp_message(p_message,'Timeout error - System Cancelled the  Prompt')
-                      }
-                    }
-                  }
-                })
-              }else{
-                sets_message("Not Supported with any biometric")
-              }
-            })
+    hasHardwareAsync.then((value)=>{if(value) seth_message("scanner is available on the device") });
+    
+    const isenrolled = LocalAuthentication.isEnrolledAsync();
+    isenrolled.then((value) => { if(value) sete_message("device has saved data to use for authentication")});
+   
+    const support = LocalAuthentication.supportedAuthenticationTypesAsync();
+    support.then((value) => { if(value) sets_message(value)});
 
+    const getEnrolled = LocalAuthentication.authenticateAsync()
+
+      getEnrolled.then((value)=> {
+        console.log(value.success)
+        if(value.success){
+          // setnavigates(true)
+          setp_message("Authenticated successfully")
+        }else{
+          // setnavigates(false)
+          setp_message("Validation Fails => ")
+          if(value.error == 'user_cancel'){
+            setp_message("User Cancelled the  Prompt")
           }else{
-            sete_message("Device Not registered with signature")
+            if(value.error == 'system_cancel'){
+              setp_message('Timeout error - System Cancelled the  Prompt')
+            }
           }
-        })
-      }else{
-        seth_message("Device Not Elegible for Biometric Auth")
-      }
-      
+        }
+      })
+ 
+  }
 
-    });
-
-    }
-
-     useEffect(()=>{
-       getBiometric()
-     }) ;
-        
-    return(
-        <View style={{ flex:1, justifyContent: "center",alignItems: "center" ,backgroundColor: "white"}}>
-            <ImageBackground 
-            style={{ width: 400, height: 400 }}
-            source = {require('../Assets/welcome.png')}>
-            </ImageBackground>  
-            
-                <Text>
-                  {h_message}
-                </Text>
-                <Text>
-                  {s_message}
-                </Text>
-                <Text>
-                  {e_message}
-                </Text>
-                <Text>
-                   {p_message}
-                </Text>
-                    
-        </View>
+    return (
+      <View style={{ flex:1, justifyContent: "center",alignItems: "center" ,backgroundColor: "white"}}>      
+        <TouchableOpacity style={styles.button}  onPress={BioCheck}><Text>Click To check</Text></TouchableOpacity>
+     
+        <Text >        
+          {h_message}{'\n'}         
+       </Text>       
+        <Text style={{  justifyContent: "center"}}>       
+          {e_message}{'\n'}        
+        </Text>
+        <Text>       
+        Device Configured With : 
+          {'\t'}{s_message} {'\n'}{'\n'} 1-FINGERPRINT,{'\n'} 2-FACIAL_RECOGNITION,{'\n'} 3-IRIS {'\n'}
+        </Text>
+        <Text>        
+          {p_message}{'\n'}       
+        </Text>
+      </View>
     )
 }
+const styles = StyleSheet.create({
+ 
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  
+});
